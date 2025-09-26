8b934b7b9aba2636a9e1524ef985f4720d4742b1
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2025-09-26 22:05:47 +0530

    Blog name updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,11 @@ export default function Navbar() {
   const {navigate, token} = useAppContext();
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer'>
-        <img onClick={()=> navigate('/')} src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' />
+        {/* <img onClick={()=> navigate('/')} src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' /> */}
+        <div className='w-35 text-center rounded-lg  cursor-pointer flex  items-center'>
+          <div className='bg-primary h-10 w-1 '></div>
+          <h1 onClick={()=> navigate('/')} className='text-2xl  pl-2 uppercase font-semibold text-gray-700 '>My Blogs</h1>
+        </div>
         <button onClick={()=> navigate('/admin')} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>{token ? 'Dashboard' : 'Login'}
             <img src={assets.arrow} alt="arrow" className='w-3' />
         </button>
